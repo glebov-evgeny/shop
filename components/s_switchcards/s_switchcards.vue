9f1b5eb109f3aa5a01c376eabea4cbe319581494
Глебов Евгений Андреевич <EGlebov@synergy.ru>
2022-11-22 16:12:45 +0300

    feat(components): fix firebase
    НОМЕР ЗАДАЧИ: main

--- components/s_switchcards/s_switchcards.vue
+++ components/s_switchcards/s_switchcards.vue
@@ -9,9 +9,8 @@
 
 <script>
 import './s_switchcards.scss';
-
 import {
-  getFirestore, collection, getDocs, onSnapshot, query,
+  getFirestore, collection, onSnapshot, query,
 } from 'firebase/firestore';
 
 export default {
@@ -24,43 +23,14 @@ export default {
   methods: {
     async fetchCards() {
       const db = getFirestore();
-      // const citiesRef = collection(db, 'cards', '');
-      // console.log(citiesRef);
-      console.log(getDocs);
-      // const querySnapshot = await getDocs(collection(db, 'cards'));
-      const getData = query(collection(db, 'cards'));
+      const getData = query(collection(db, 'cards/usa/list'));
       onSnapshot(getData, (querySnapshot) => {
         const arr = [];
         querySnapshot.forEach((doc) => {
           arr.push(doc.data());
         });
-
         this.cards = arr;
       });
-      // querySnapshot.forEach((doc) => {
-      //   // this.cards.push(doc.data());
-      //   console.log(`${doc.data()}`);
-      //   // this.cards = `${doc.id} => ${doc.data()}`;
-      // });
-
-      // eslint-disable-next-line no-undef
-      // return axios.get('https://shop-972a3.firebaseio.com/users.json');
-      //   const db = getFirestore();
-
-      //   console.log(db);
-      // //   const db = getFirestore();
-      //   console.log(db);
-      // const productsSnap = await db.collection('products').get();
-      // const productsData = productsSnap.docs.map((doc) => ({
-      //   uuid: doc.id,
-      //   ...doc.data(),
-      // }));
-      // return productsData;
-      // const querySnapshot = await getDocs(collection(db, 'cards'));
-      // console.log(querySnapshot);
-      // querySnapshot.forEach((doc) => {
-      //   console.log(`${doc.id} => ${doc.data()}`);
-      // });
     },
   },
   mounted() {
