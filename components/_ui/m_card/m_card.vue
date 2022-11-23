20f5f326e7e1cfcfdb9afe083c47edc322b3abcb
Глебов Евгений Андреевич <EGlebov@synergy.ru>
2022-11-23 17:05:17 +0300

    feat(components): add
    main
    
    НОМЕР ЗАДАЧИ: main

---
+++ components/_ui/m_card/m_card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+import './m_card.scss';
+
+export default {
+  name: 'MCard',
+  props: {
+    name: {
+      type: String,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
--- components/s_switchcards/s_switchcards.vue
+++ components/s_switchcards/s_switchcards.vue
@@ -3,17 +3,27 @@
     <div class="container switchcards__container">
       <h2 class="switchcards__title title" v-html="$t('title')"></h2>
       <div class="switchcards__region-box">
-        <p class="switchcards__region-title">Выбор региона: {{ regionsCurrent }}</p>
-        <div class="switchcards__region-toggle" @click="toggleListHandler">
-          <p class="switchcards__region-current" :data-current="regionsCurrent">{{ regionsCurrent }}</p>
-          <div class="switchcards__region-list" :class="[toggleList ? '_active' : '']" @click.stop="toggleOption">
-            <p class="switchcards__region-option _current" data-option="usa">usa</p>
-            <p class="switchcards__region-option" data-option="pol">pol</p>
-            <p class="switchcards__region-option" data-option="bra">bra</p>
+        <p class="switchcards__region-title">Выбор региона:</p>
+        <div class="switchcards__region-select">
+          <div class="switchcards__region-toggle" @click="toggleListHandler">
+            <p class="switchcards__region-current" :data-current="regionsCurrent">{{ regionsCurrent }}</p>
+          </div>
+          <div class="switchcards__region-list" :class="[toggleList ? '_active' : '']" @click="toggleOption">
+            <p
+              v-for="regions in regionsList"
+              class="switchcards__region-option"
+              :class="regions.currentClass"
+              :data-option="regions.code"
+              :key="regions.code"
+            >
+              {{ regions.code }}
+            </p>
           </div>
         </div>
       </div>
-      <div>{{ cards }}</div>
+      <div class="switchcards__region-block">
+        <m-card v-for="card in cards" :key="card.name" :name="card.name" />
+      </div>
     </div>
   </section>
 </template>
@@ -24,12 +34,30 @@ import {
   getFirestore, collection, onSnapshot, query,
 } from 'firebase/firestore';
 
+import MCard from '@/components/_ui/m_card/m_card';
+
 export default {
   name: 's-switchcards',
+  components: {
+    MCard,
+  },
   data() {
     return {
       cards: [],
-      regionsList: ['usa', 'pol'],
+      regionsList: [
+        {
+          code: 'usa',
+          currentClass: '_current',
+        },
+        {
+          code: 'pol',
+          currentClass: '',
+        },
+        {
+          code: 'bra',
+          currentClass: '',
+        },
+      ],
       regionsCurrent: 'usa',
       toggleList: false,
     };
@@ -37,7 +65,7 @@ export default {
   methods: {
     async fetchCards() {
       const db = getFirestore();
-      const getData = query(collection(db, 'cards/usa/list'));
+      const getData = query(collection(db, `cards/${this.regionsCurrent}/list`));
       onSnapshot(getData, (querySnapshot) => {
         const arr = [];
         querySnapshot.forEach((doc) => {
@@ -50,8 +78,14 @@ export default {
       this.toggleList = !this.toggleList;
     },
     toggleOption(event) {
+      const allItems = document.querySelectorAll('.switchcards__region-option');
+      allItems.forEach((element) => {
+        element.classList.remove('_current');
+      });
+      event.target.classList.add('_current');
       this.regionsCurrent = event.target.getAttribute('data-option');
       this.toggleList = false;
+      this.fetchCards();
     },
   },
   mounted() {
