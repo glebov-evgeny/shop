40910af742959e6d8609d10d5473a0d269d7820c
Глебов Евгений Андреевич <EGlebov@synergy.ru>
2022-12-19 11:53:20 +0300

    6f7f900

---
+++ _nuxt/static/1671439990/en/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1671439990",layout:"default",error:a,state:{token:a,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fen",config:{SITE_URL:"http:\u002F\u002Flocalhost",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1671439990/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1671439990",layout:"default",error:a,state:{token:a,i18n:{routeParams:{}}},serverRendered:true,routePath:b,config:{SITE_URL:"http:\u002F\u002Flocalhost",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
